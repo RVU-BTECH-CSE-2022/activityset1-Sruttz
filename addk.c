43c34468d622d4a8b3985f1d8a659b4f979696cd
Sruthika Sivakumar <110161415+Sruttz@users.noreply.github.com>
2022-08-11 09:03:55 +0000

    new commit

---
+++ addk.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int find_largest (int a,int b)
+{
+int c;
+if(a<b) 
+{
+c=b;
+}
+else
+{
+c=a;
+}
+return c;
+}
--- p0original.c
+++ p0original.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 int main(void)
 {
-  printf("My name is Sruthika\n");
+  float length=10.0, breadth=20.0;
+  float area=0.5*length*breadth;
+  printf("the area of the triangle having length=%f and breadth=%f is=%f", length,breadth,area);
   return 0;
-}
+  }
